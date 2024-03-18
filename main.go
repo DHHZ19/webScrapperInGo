d188018427dcc81c2d805f2dc28b8f2cc0523093
Diego Hernandez <diegofootball19@gmail.com>
2024-03-18 18:33:41 -0500

    update courses complete

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -18,25 +19,33 @@ type Course struct {
 
 func main() {
 	// Where I will put the json file of frontend masters courses
-	// fName := "courses.json"
-	// file, err := os.Create(fName)
-	// if err != nil {
-	// 	log.Fatalf("Cannot create file %q: %s\n", fName, err)
-	// 	return
-	// }
-	// defer file.Close()
+	fName := "courses.json"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
 
-	c := colly.NewCollector(colly.AllowedDomains("frontendmasters.com"))
+	// Create a channel to signal when the goroutine is done
+	done := make(chan struct{})
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("frontendmasters.com"),
+
+		// Cache responses to prevent multiple download of pages
+		// even if the collector is restarted
+		colly.CacheDir("./courses_cache"),
+	)
 
 	courses := make([]Course, 0, 200)
 
 	c.OnHTML("div.FM-Course-Item-Content", func(e *colly.HTMLElement) {
-		course := Course{
-			Title: e.Text,
-		}
+		var course Course
 
 		// Iterate over div components and add details to course
 		e.ForEach("div > .FM-Heading-3 .FM-Link", func(_ int, el *colly.HTMLElement) {
+			course.Title = el.Text
 			course.URL = fmt.Sprintf("%s%s", "https:/frontendmasters.com", el.Attr("href"))
 		})
 
@@ -47,23 +56,20 @@ func main() {
 		e.ForEach("div > .description", func(_ int, el *colly.HTMLElement) {
 			course.Description = el.Text
 		})
-		// fmt.Println(course)
 		courses = append(courses, course)
-		// fmt.Println(courses)
 	})
-	jsonBytes, err := json.Marshal(courses)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(string(jsonBytes))
-	// // Find and visit all links
-	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-	// 	e.Request.Visit(e.Attr("href"))
-	// })
+	// Start the goroutine
+	go func() {
+		defer close(done) // Signal that the goroutine is done
+		c.Visit("https://frontendmasters.com/courses")
+	}()
 
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Visiting", r.URL)
-	// })
+	// Wait for the goroutine to finish
+	<-done
 
-	c.Visit("https://frontendmasters.com/courses")
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", "  ")
+
+	// Dump json to the standard output
+	enc.Encode(courses)
 }
